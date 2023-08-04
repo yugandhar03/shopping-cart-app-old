07bfb2174b391d094d597dc30245493f753be2f3
parimala G <parimala@discernsecurity.com>
2023-08-04 23:28:08 +0530

    initial setup

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser =require('body-parser');
+const mongoose =require('mongoose');
+const cors =require('cors');
+const dotenv =require('dotenv');
+
+dotenv.config();
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
